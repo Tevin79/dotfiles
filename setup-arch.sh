02afc1bb06901741cce63aee2156a42a875e621d
Tevin79 <wtevin2015@gmail.com>
2025-05-13 19:49:44 +0200

    some conf files

--- setup-arch.sh
+++ setup-arch.sh
@@ -1,9 +1,50 @@
+#!/bin/bash
+
 # ==============================
 # ==========Arch linux==========
 # ==============================
-sudo pacman -Syu && sudo pacman -S neovim git texlive
-# to have tree-sitter in neovim
-sudo npm install -g tree-sitter-cli
+
+for param in $@
+do
+	if [ $param = "-i" -o $param = "all" ]
+	then
+		echo "[+] Package installation..."
+		sudo pacman -Syu && sudo pacman -S starship neovim git texlive npm stow fastfetch btop firefox nerd-fonts tmux hyprlock --noconfirm --needed
+		echo "[+] Packages installation complete"
+	fi
+	if [ $param = "-npm" -o $param = "all" ]
+	then
+		echo "[+] Installing tree-sitter for neovim..." 
+		# to have tree-sitter in neovim
+		sudo npm install -g tree-sitter-cli
+		echo "[+] Tree-sitter installation complete"
+	fi
+	if [ $param = "-f" -o $param = "all" ]
+	then
+		echo "[+] Updating submodules..."
+		git submodule update --init
+		echo "[+] Submodules updated"
+		echo "[+] Stow bashrc"
+		rm ../.bashrc && stow --dotfiles bashrc
+		echo "[+] Stow kitty"
+		rm ../.config/kitty && stow --dotfiles kitty
+		echo "[+] Stow hyprland"
+		rm ../.config/hypr && stow --dotfiles hyprland
+		echo "[+] Stow tmux"
+		rm ../.config/tmux && stow --dotfiles tmux
+		echo "[+] Setup starship"
+		starship preset gruvbox-rainbow -o ~/.config/starship.toml
+	fi
+done
+if [ $# -eq 0 ]
+then
+	echo "-i   : to install packages"
+	echo "-f   : to stow dotfiles"
+	echo "-npm : to install npm things"
+	echo "all  : do everything above"
+else
+	echo "[+] Configuration complete"
+fi
 
 # ==============================
 # ============GLOBAL============
